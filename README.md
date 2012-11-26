ce4bb7f75e35d0fcf5a7dc0801ed8ec50c964d4b
Joseph Furlott <jmfurlott@gmail.com>
2012-11-26 02:05:15 -0500

    updated readme and cleaned code

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 
-11/25/2012
+
 Joseph Furlott
 
+11/26/12 - 0.3
+-thickness around veins
+
+
+11/26/12 - 0.2
+-colors: red and blue
+
+
+11/25/12 - 0.1
 Draws lines from a textfile (new line created everytime - needs to be improved)
 -no camera yet
 -no colors (artery vs vein)
--- test.cpp
+++ test.cpp
@@ -1,6 +1,12 @@
 //Joseph Furlott
 //Help from www.swiftless.com tutorials - needs proper credits
 
+/*TODO:
+ -menu system
+ -camera and movement
+ 
+
+*/
 
 #include <GLUT/glut.h>
 #include <iostream>
@@ -12,23 +18,16 @@ using namespace std;
 
 
 bool* keyStates = new bool[256];  //holds all the key values; 0 is not pressed, 1 is pressed
-
-
-//variables to handle rotation and transformations
-bool movingUp = false;
-float yLocation = 0.0f;
-float yRotationAngle = 0.0f;
 vector<float> coordinates(1);
-vector<float> xx(1);
-vector<float> yy(1);
-vector<float> zz(1);
 int size;
 
 
+
+//-------------------------------------------------------------get from text file
 //to read in the coordinate file
 int readCoordinates() {
     //first need to open the file
-    ifstream file("Coordinates10.txt");
+    ifstream file("CoordinatesAll.txt");
     float temp;
     string tempDiameter;
     string tempColor;
@@ -44,12 +43,6 @@ int readCoordinates() {
             file >> temp;
             coordinates[i] = temp/1000;
             i += 1;
-//            counter += 1;
-//            if(counter > 5) {
-//                counter = 0;
-//                file >> tempDiameter;
-//                file >> tempColor; //tested and working
-//            }
             coordinates.resize(i+2); //dynamically resize the vector such that it is always one ahead
         }
         
@@ -57,64 +50,18 @@ int readCoordinates() {
         //so now the vector should hold the appropriate data
         //STILL NEED TO CONVERT TO FLOAT but just testing for now
         
-        
-            
+        return 1;            
     }
 	else {
 		 cout << "Unable to open file";
         return 0;
     }
-    
-//    xx.resize(size/3);
-//    int a = 0;
-//    for(int j = 0; j < size/3; j+=3) {
-//        xx[a] = coordinates[j];
-//     //   cout << xx[a] << endl;
-//        a++;
-//    }
-//    
-//    yy.resize(size/3);
-//    a = 0;
-//    for(int j = 1; j < size/3; j+=3) {
-//        yy[a] = coordinates[j];
-//        
-//        a++;
-//    }
-//    
-//    zz.resize(size/3);
-//    a = 0;
-//    for(int j = 2; j < size/3; j+=3) {
-//        zz[a] = coordinates[j];
-//        
-//        a++;
-//    }
-
-    return 1;
-
 
 }
 
 
 
-//method to convert the vector to a GLint array (primitive)
-//not sure if static
-void convertVectorToArray() {
-    
-    int good = readCoordinates();
-    size = coordinates.size();
-    //first build the array
-    GLfloat vertices[size];
-    
-    for(int i = 0; i < size; i++) {
-        vertices[i] = coordinates[i];
-    }
-    
-    //now make a pointer to that built array in GLint form
-    GLfloat* a = vertices;
-    
-}
-
-
+//-------------------------------------------------------------reshapes the window size (no objects)
 
 
 void reshape(int width, int height) {
@@ -132,6 +79,10 @@ void reshape(int width, int height) {
 	glMatrixMode(GL_MODELVIEW);
 	
 }
+
+
+//-------------------------------------------------------------how to handle key presses
+
 
 void keyPressed(unsigned char key, int x, int y) {
 	if(key == 'a') {
@@ -156,80 +107,20 @@ void keyOperations(void) {
 
 
 
-//drawing a square in space
-void renderPrimitive(void) {
+
+
+//-------------------------------------------------------------rending the eye
+
+//the actual rendering method that gets the vertices and draws the lines
+void renderEye(void) {
 	//start the beginning of the opengl call with glBegin
 
     int nothing = readCoordinates();
 	//though there is more performance to use triangles with five vertices
-//	
-//    glEnableClientState(GL_VERTEX_ARRAY);
-//    
-////    //pulling from vector! only one dimension; slow as shit
-////    //TODO:optimization
-////    int i = 0;
-////    int x1, y1, z1;
-////    int x2, y2, z2;
-////    while (i < coordinates.size()) {
-////        
-////        glBegin(GL_LINES);
-////        x1 = coordinates[i];
-////        y1 = coordinates[i+1];
-////        z1 = coordinates[i+2];
-////        x2 = coordinates[i+3];
-////        y2 = coordinates[i+4];
-////        z2 = coordinates[i+5];
-////        i = i + 8; //skipping color and dimeter for now
-////        
-////        //now do the drawing
-////        cout << x1;
-////        glVertex3f(x1,y1,z1);
-////        glVertex3f(x2,y2,z2);
-////        glEnd();
-////    }
-//    
-//    
-//    //----------------------------------------------
-//    GLfloat x_vertices[size/3];
-//    GLfloat y_vertices[size/3];
-//    GLfloat z_vertices[size/3];
-//    int a = 0;
-//    for(int j = 0; j < size/3; j++) {
-//        x_vertices[j] = xx[j];
-//        //cout << x_vertices[j] << endl;
-//
-//    }
-//    
-//    for(int j = 0; j < size/3; j++) {
-//        y_vertices[j] = yy[j];
-//        //cout << y_vertices[j] << endl;
-//        
-//    }
-//    
-//
-//    for(int j = 0; j < size/3; j++) {
-//        z_vertices[j] = zz[j];
-//       // cout << z_vertices[j] << endl;
-//        
-//    }
 
-//    
-//    
-//    glVertexPointer(3, GL_FLOAT, 0, x_vertices);
-//    glVertexPointer(3, GL_FLOAT, 0, y_vertices);
-//    glVertexPointer(3, GL_FLOAT, 0, z_vertices);
-
-    //and now everything is loaded so draw the lines!
-
-    //now set the diameters
-    //glLineWidth(10);
-    //width only changes outside of glBegin but why??
     
-    
-    
-    //glColor3f(1.0,0.0,0.0); //red
     float tempColor;
-    GLfloat tempDiameter; //not used yet
+    GLfloat tempDiameter;
     for(int i = 0; i < size ; i+=8) {
      //   glVertex3f(x_vertices[i],y_vertices[i],z_vertices[i]);
       //  glVertex3f(x_vertices[i+1],y_vertices[i+1],z_vertices[i+1]);
@@ -255,10 +146,12 @@ void renderPrimitive(void) {
         glVertex3f(coordinates[i+3],coordinates[i+4], coordinates[i+5]);
         glEnd();
     }
-//    glEnd();
+
     
     
 }
+
+//-------------------------------------------------------------the main window
 
 void display(void) {
 	keyOperations(); //should be done first always otherwise key presses could mess with the rendering
@@ -268,34 +161,17 @@ void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 	
-	glTranslatef(-1.5f, -1.0f, -5.0f); //push the scene back 5z that way can see everything (0,0,0) -> (0,0,-5)
-	renderPrimitive(); //method that is drawing the square
-	
-	//handles the moving of the cube
-	//order counts!
-//	glTranslatef(0.0f, yLocation, 0.0f);
-//	glRotatef(yRotationAngle, 0.0f, 1.0f, 0.0f);
-	
-//	glutWireCube(2.0f);
+	glTranslatef(-1.5f, -1.0f, -5.0f); //push the scene back that way can see everything
+	renderEye(); 
+
 
 	glFlush();
-/*	
-	//bouncing cube example:::
-	if (movingUp)	
-		yLocation -= 0.005f;
-	else
-		yLocation += 0.005f;
-		
-	if(yLocation < -3.0f)
-		movingUp = false;
-	else if(yLocation > 3.0f)
-		movingUp = true;
-		
-	yRotationAngle += 0.0005f;
-	if(yRotationAngle > 360.0f)
-		yRotationAngle -= 360.0f;
-*/
+
 }
+
+
+
+//-------------------------------------------------------------main method-what is running
 
 int main(int argc, char **argv) {
 	
